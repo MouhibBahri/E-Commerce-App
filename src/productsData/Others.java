6b3ff258a56b795980c68be6da820e2b605f42b6
MouhibBahri <mouhib.bahri@insat.ucar.tn>
2023-12-26 19:49:01 +0100

    Initial commit

---
+++ src/productsData/Others.java
@@ -0,0 +1,12 @@
+package productsData;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+
+public class Others extends ProductModel {
+    public void writeSpecificAttributes(BufferedWriter writer) {
+    }
+
+    public void readSpecificAttributes(BufferedReader reader) {
+    }
+}
